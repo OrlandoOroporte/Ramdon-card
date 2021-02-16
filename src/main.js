e9062b7d2a83b7935ddf48e02db7690f44ee9a37
Brandchal <Bchavarrial@hotmail.com>
2021-02-16 03:44:37 +0000

    update

--- src/main.js
+++ src/main.js
@@ -6,5 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let simbolo = ["♦", "♠", "♣", "♥"];
+  let sym = Math.floor(Math.random() * simbolo.length);
+  let symTop = simbolo[sym];
+  let symBotton = simbolo[sym];
+
+  let num = Math.floor(Math.random() * numero.length);
+
+  document.querySelector("#symbol1").innerHTML = symTop;
+  document.querySelector("#number").innerHTML = num;
+  document.querySelector("#symbol2").innerHTML = symBotton;
 };
