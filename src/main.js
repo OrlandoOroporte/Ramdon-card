a7b83f507ca1539a81f1bec90af5b80be8279075
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-10-11 14:20:10 +0000

    First commit

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
   let simbolo = ["♦", "♠", "♣", "♥"];
-  //Método random que recorre el vecto para elegir un simbolo.
-  let symA = Math.floor(Math.random() * simbolo.length); //Se utiliza esta variable para asignar a dos varibles que van a ser iguales.
+
+  let symA = Math.floor(Math.random() * simbolo.length);
   let symTop = simbolo[symA];
   let symBotton = simbolo[symA];
 
@@ -20,7 +20,6 @@ window.onload = function() {
   document.querySelector("#number").innerHTML = num;
   document.querySelector("#symbol2").innerHTML = symBotton;
 
-  //Método para cambiar de color los simbolos, cuando sean diamante o corazón.
   if (symTop == simbolo[0] || symBotton == simbolo[3]) {
     document.getElementById("symbol1").style.color = "red";
     document.getElementById("symbol2").style.color = "red";
